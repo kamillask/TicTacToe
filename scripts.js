e7233db52a36b834d21ee213653adb03fa6e7550
kamillask <kamil.laskowski86@qmail.cuny.edu>
2024-10-23 11:38:12 -0400

    Finished win logic.

--- scripts.js
+++ scripts.js
@@ -1,27 +1,28 @@
 function createPlayer(name, symbol) {
     symbol: symbol;
     name: name;
+    const getWinningString = () => symbol + symbol + symbol;
     const getName = () => name;
     const getSymbol = () => symbol;
-    return {getName, getSymbol};
+    return { getName, getSymbol, getWinningString };
 }
 
-
-const Gameboard = (function() {
+const Gameboard = (function () {
     let board = [
+        [" ", " ", "x"],
         [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "]
+        ["x", " ", " "],
     ];
 
-    const displayBoard = () => board.forEach((row) => console.log(row.join("|")));
+    const displayBoard = () =>
+        board.forEach((row) => console.log(row.join("|")));
 
     const getSquare = (row, column) => board[row][column];
 
     const setSquare = (row, column, player) => {
-        if(board[row][column]===" "){
+        if (board[row][column] === " ") {
             board[row][column] = player.getSymbol();
-        } else{
+        } else {
             alert("Space already occupied.");
         }
     };
@@ -30,14 +31,55 @@ const Gameboard = (function() {
         board = [
             [" ", " ", " "],
             [" ", " ", " "],
-            [" ", " ", " "]
+            [" ", " ", " "],
         ];
-    }
+    };
 
-    // const checkWin = () => ;
+    const checkRows = (player) => {
+        for (let row in board) {
+            if (board[row].join("") === player.getWinningString()) {
+                return true;
+            }
+        }
+    };
 
-    return {displayBoard, getSquare, setSquare, resetBoard};
+    const checkColumns = (player) => {
+        for (let row in board) {
+            let columnString = "";
+            for (let col = 0; col < board.length; col++) {
+                columnString += board[col][row];
+            }
+            if (columnString === player.getWinningString()) {
+                return true;
+            }
+        }
+    };
 
+    const checkDiagonals = (player) => {
+        let diagonalStringLtR = "";
+        let diagonalStringRtL = "";
+        for(let row in board){
+            diagonalStringLtR+=board[row][row];
+            diagonalStringRtL+=board[row][board.length-1-row];
+        }
+        if(diagonalStringLtR===player.getWinningString() || diagonalStringRtL===player.getWinningString()){
+            return true;
+        }
+    };
+
+    const checkWin = (player) => {
+        if (checkRows(player) || checkColumns(player) || checkDiagonals(player)) {
+            return true;
+        }
+    };
+
+    return {
+        displayBoard,
+        getSquare,
+        setSquare,
+        resetBoard,
+        checkWin,
+    };
 })();
 
 //iife
@@ -47,27 +89,29 @@ const Gameboard = (function() {
     const player1 = createPlayer(player1name, "x");
     const player2 = createPlayer(player2name, "o");
     const players = [player1, player2];
-    
+
     let currentPlayer = players[0];
     const getCurrent = () => currentPlayer;
 
     const switchPlayer = () => {
-        if(currentPlayer===players[0]){
-            currentPlayer=players[1];
-        } else{
-            currentPlayer=players[0];
+        if (currentPlayer === players[0]) {
+            currentPlayer = players[1];
+        } else {
+            currentPlayer = players[0];
         }
-    }
+    };
 
-    while(true){
+    while (true) {
         //wrap all this in a function, returns true if game not won, false if won
         Gameboard.displayBoard();
         console.log(getCurrent().getName() + "'s turn");
         const row = prompt("Enter row");
         const column = prompt("Enter column.");
         Gameboard.setSquare(row, column, getCurrent());
-        Gameboard.displayBoard();
+        if (Gameboard.checkWin(getCurrent())) {
+            alert("win");
+            break;
+        }
         switchPlayer();
     }
-    
 })();
